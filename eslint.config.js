8e4b20fae3fd0640550ae3adec6473f5d6f5f27f
Krishna Godavari <229684683+krishna-godavari@users.noreply.github.com>
2026-01-02 18:42:30 +0530

    Update configuration

--- eslint.config.js
+++ eslint.config.js
@@ -4,22 +4,25 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 import globals from 'globals';
 
 export default [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...eslintPluginAstro.configs.recommended,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
-        },
-        rules: {
-            '@typescript-eslint/no-unused-vars': [
-                'warn',
-                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-            ],
-            '@typescript-eslint/no-explicit-any': 'warn',
-        },
+  {
+    ignores: ['docs/**', '.astro/**', 'node_modules/**'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-]
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
