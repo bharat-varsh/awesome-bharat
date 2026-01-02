5bbddfbf12452f6aea53aedd8e176cf13ea49b95
Krishna Godavari <229684683+krishna-godavari@users.noreply.github.com>
2026-01-02 16:07:19 +0530

    Add a different palette of blue as secondary

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,17 +6,29 @@ export default {
         extend: {
             colors: {
                 primary: {
-                50:  '#fff7ed', // very light kesar
+                50:  '#fff7ed',
                 100: '#ffedd5',
                 200: '#fed7aa',
                 300: '#fdba74',
                 400: '#fb923c',
-                500: '#f97316', // classic saffron
+                500: '#f97316',
                 600: '#ea580c',
                 700: '#c2410c',
                 800: '#9a3412',
-                900: '#7c2d12', // deep temple-orange
+                900: '#7c2d12',
                 },
+                secondary: {
+                    50:  '#f8fafc',
+                    100: '#f1f5f9',
+                    200: '#e2e8f0',
+                    300: '#cbd5e1',
+                    400: '#94a3b8',
+                    500: '#64748b',
+                    600: '#475569',
+                    700: '#1e40af',
+                    800: '#1e3a8a',
+                    900: '#172554',
+                    }
             },
             typography: (theme) => ({
                 DEFAULT: {
